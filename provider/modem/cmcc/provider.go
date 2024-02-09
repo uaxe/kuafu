e2f17729a4228179061dc472b5c9be70823d35a2
zkep <773683464@qq.com>
2024-02-09 19:31:10 +0800

    update telnet client

--- provider/modem/cmcc/provider.go
+++ provider/modem/cmcc/provider.go
@@ -2,15 +2,8 @@ package cmcc
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
-	"regexp"
-	"strings"
-
-	"github.com/uaxe/infra/cmd"
 	"github.com/uaxe/kuafu/provider/modem"
+	"net/http"
 )
 
 func init() {
@@ -33,9 +26,9 @@ type options struct {
 	ctx       context.Context
 	hclient   *http.Client
 	host      string
+	port      int
 	macAddr   string
 	telnetURL string
-	port      int
 }
 
 func WithContext(ctx context.Context) OptionFunc {
@@ -94,7 +87,7 @@ func (s *CMCCProvider) New(ctx context.Context, f *modem.AdminFlag) (modem.Provi
 	if f.MacAddr != "" {
 		ss.macAddr = f.MacAddr
 	}
-	if f.Port > 0 {
+	if f.Port != 0 {
 		ss.port = f.Port
 	}
 	return ss, nil
@@ -102,71 +95,4 @@ func (s *CMCCProvider) New(ctx context.Context, f *modem.AdminFlag) (modem.Provi
 
 func (s *CMCCProvider) Type() string {
 	return s.providerType
-}
-
-func (s *CMCCProvider) GetSuperAdmin() (*modem.SuperAdmin, error) {
-	macAddr, err := s.arp()
-	if err != nil {
-		return nil, err
-	}
-
-	ret := modem.SuperAdmin{MacAddr: macAddr}
-
-	if err = s.telnetEnable(macAddr); err != nil {
-		return nil, err
-	}
-
-	return &ret, nil
-}
-
-var (
-	RegARP = regexp.MustCompile(`\((\d{1,3}\.){3}\d{1,3}\)\s{1,2}at\s{1,2}([\d|\w]{1,3}:){5}[\d|\w]{1,3}`)
-
-	RegIP = regexp.MustCompile(`(\d{1,3}\.){3}\d{1,3}`)
-
-	RegMAC = regexp.MustCompile(`([\d|\w]{1,3}:){5}[\d|\w]{1,3}`)
-)
-
-func (s *CMCCProvider) arp() (string, error) {
-	output, err := cmd.NewExecCmd(s.options.ctx, cmd.WithName("arp"), cmd.WithArgs("-a")).CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-
-	arps := RegARP.FindAllString(string(output), -1)
-	for _, value := range arps {
-		if ip := RegIP.FindString(value); ip == s.host {
-			return RegMAC.FindString(value), nil
-		}
-	}
-
-	return "", fmt.Errorf("not found host %s", s.host)
-}
-
-func (s *CMCCProvider) telnetEnable(macAddr string) error {
-	resp, err := s.hclient.Get(fmt.Sprintf(s.telnetURL, s.host, macAddr))
-	if err != nil {
-		return err
-	}
-
-	defer func() { _ = resp.Body.Close() }()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("telnet start http not OK")
-	}
-
-	raw, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	if !strings.Contains(string(raw), "telnet") {
-		return errors.New(string(raw))
-	}
-
-	return nil
-}
-
-func (s *CMCCProvider) telnetAdminAndPwd() error {
-
-	return nil
 }
---
+++ provider/modem/cmcc/super_admin.go
@@ -0,0 +1,232 @@
+package cmcc
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/uaxe/infra/cmd"
+	"github.com/uaxe/kuafu/provider/modem"
+)
+
+func (s *CMCCProvider) GetSuperAdmin() (*modem.SuperAdmin, error) {
+	ret := modem.SuperAdmin{}
+	macAddr, err := s.arp()
+	if err != nil {
+		return nil, err
+	}
+	ret.MacAddr = macAddr
+
+	telnetMacAddr := ""
+	for _, part := range strings.Split(macAddr, ":") {
+		if len(part) == 1 {
+			part = fmt.Sprintf("0%v", part)
+		}
+		telnetMacAddr += strings.ToUpper(part)
+	}
+
+	if err = s.telnetEnable(telnetMacAddr); err != nil {
+		return nil, err
+	}
+
+	if err = s.telnetAdminAndPwd(telnetMacAddr, &ret); err != nil {
+		return nil, err
+	}
+
+	return &ret, nil
+}
+
+var (
+	RegIP = regexp.MustCompile(`(\d{1,3}\.){3}\d{1,3}`)
+
+	RegMAC = regexp.MustCompile(`([\d|\w]{1,3}:){5}[\d|\w]{1,3}`)
+)
+
+func (s *CMCCProvider) arp() (string, error) {
+	output, err := cmd.NewExecCmd(s.options.ctx, cmd.WithName("arp"), cmd.WithArgs("-a")).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		value := scanner.Text()
+		if ip := RegIP.FindString(value); ip == s.host {
+			return RegMAC.FindString(value), nil
+		}
+	}
+
+	return "", fmt.Errorf("not found host %s", s.host)
+}
+
+func (s *CMCCProvider) telnetEnable(macAddr string) error {
+	uri := fmt.Sprintf(s.telnetURL, s.host, macAddr)
+	resp, err := s.hclient.Get(uri)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("telnet start http not OK")
+	}
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(string(raw), "telnet") {
+		return fmt.Errorf("%s %s", uri, string(raw))
+	}
+
+	return nil
+}
+
+func (s *CMCCProvider) telnetAdminAndPwd(macAddr string, ret *modem.SuperAdmin) error {
+	addr := fmt.Sprintf("%s:%d", s.host, s.port)
+	conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = conn.Close() }()
+
+	if len(macAddr) < 6 {
+		return fmt.Errorf("mac addr not less size %s", macAddr)
+	}
+
+	uname := "admin"
+	pwd := fmt.Sprintf("Fh@%s", macAddr[len(macAddr)-6:])
+
+	var buf [4096]byte
+	n, err := conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	buf[1] = 252
+	buf[4] = 252
+	buf[7] = 252
+	buf[10] = 252
+	n, err = conn.Write(buf[0:n])
+	if err != nil {
+		return err
+	}
+
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(string(buf[0:]), "Login:") {
+		buf[1] = 252
+		buf[4] = 251
+		buf[7] = 252
+		buf[10] = 254
+		buf[13] = 252
+
+		n, err = conn.Write(buf[0:n])
+		if err != nil {
+			return err
+		}
+
+		n, err = conn.Read(buf[0:])
+		if err != nil {
+			return err
+		}
+
+		buf[1] = 252
+		buf[4] = 252
+		n, err = conn.Write(buf[0:n])
+		if err != nil {
+			return err
+		}
+
+		n, err = conn.Read(buf[0:])
+		if err != nil {
+			return err
+		}
+	}
+
+	n, err = conn.Write([]byte(uname + "\n"))
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	n, err = conn.Write([]byte(pwd + "\n"))
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	n, err = conn.Write([]byte("load_cli factory\n"))
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	n, err = conn.Write([]byte("show admin_name\n"))
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+	unames := RegAdminName.FindStringSubmatch(string(buf[0:n]))
+	if len(unames) > 0 {
+		ret.Name = unames[1]
+	}
+
+	n, err = conn.Write([]byte("show admin_pwd\n"))
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	n, err = conn.Read(buf[0:])
+	if err != nil {
+		return err
+	}
+	pwds := RegAdminPwd.FindStringSubmatch(string(buf[0:n]))
+	if len(pwds) > 0 {
+		ret.Pwd = pwds[1]
+	}
+
+	return nil
+}
+
+var (
+	RegAdminName = regexp.MustCompile(`admin_name=(.*)`)
+
+	RegAdminPwd = regexp.MustCompile(`admin_pwd=(.*)`)
+)
+
+const (
+	TIME_DELAY_AFTER_WRITE = 500
+)
